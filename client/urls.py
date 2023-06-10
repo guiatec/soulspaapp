2f0cfcf4e0c013f97778ece26ac65a8819f1a75a
Jhon Builes <ferney14j@gmail.com>
2023-06-09 19:43:24 -0500

    first commit

---
+++ client/urls.py
@@ -0,0 +1,38 @@
+# Django
+from django.urls import path
+
+# View
+from . import views
+
+
+app_name = 'profileclient'
+
+urlpatterns = [
+
+    path(
+        route='signup_client/',
+        view=views.CreateProfileClient.as_view(),
+        name='signup_client'
+    ),
+    path(
+        route='list/clients/',
+        view=views.ListViewClient.as_view(),
+        name='list_client'
+    ),
+    path(
+        route='update/client/<int:id>/',
+        view=views.UpdateProfileClient.as_view(),
+        name='update_client'
+    ),
+    path(
+        route='update/history/client/<int:id>/',
+        view=views.ListHistoryClient.as_view(),
+        name='update_client_history'
+    ),
+    path(
+        route='delete/client/<int:id>/',
+        view=views.DeleteProfileClient.as_view(),
+        name='delete_client'
+    ),
+    
+]
